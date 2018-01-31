24010e33b792e80742d4db0ae0e5e024f160bb69
pranomostro <pranomostro@posteo.net>
2018-01-31 09:10:46 +0100

    Übungsleitung hinzugefügt.

--- pgdp/12/korrektur/Main.java
+++ pgdp/12/korrektur/Main.java
@@ -8,9 +8,9 @@ public class Main {
 		begin=new Buffer(Helper.NEXAMS);
 		end=new Buffer(Helper.NEXAMS);
 
-		mid=new Buffer[Helper.NTASKS-1];
+		mid=new Buffer[Helper.NTASKS];
 
-		for(int i=0; i<Helper.NTASKS-1; i++)
+		for(int i=0; i<Helper.NTASKS; i++)
 			mid[i]=new Buffer(Helper.BUFCAP);
 
 		exams=new Klausur[Helper.NEXAMS];
@@ -30,17 +30,21 @@ public class Main {
 			if(i%(Helper.NTASKS)==0) {
 				//MiniJava.writeConsole("Using the first buffer, writing to " + i%(Helper.NTASKS) + "\n");
 				tutors[i]=new Tutor(i%(Helper.NTASKS), begin, mid[i%(Helper.NTASKS)]);
-			} else if(i%(Helper.NTASKS)==Helper.NTASKS-1) {
-				//MiniJava.writeConsole("Using the last buffer, reading from " + (i%(Helper.NTASKS)-1) + "\n");
-				tutors[i]=new Tutor(i%(Helper.NTASKS), mid[i%(Helper.NTASKS)-1], end);
 			} else {
 				//MiniJava.writeConsole("Adding tutor consuming task " + (i%(Helper.NTASKS)-1) + "\n");
 				tutors[i]=new Tutor(i%(Helper.NTASKS), mid[i%(Helper.NTASKS)-1], mid[i%(Helper.NTASKS)]);
 			}
 		}
 
+		Uebungsleitung u1, u2;
+		u1=new Uebungsleitung(mid[Helper.NTASKS-1], end);
+		u2=new Uebungsleitung(mid[Helper.NTASKS-1], end);
+
 		for(int i=0; i<Helper.NTUTORS; i++)
 			tutors[i].start();
+
+		u1.start();
+		u2.start();
 
 		for(int i=0; i<Helper.NEXAMS; i++) {
 			try { MiniJava.writeConsole(i + ": " + end.consume().toString() + "\n"); }
--- pgdp/12/korrektur/Uebungsleitung.java
+++ pgdp/12/korrektur/Uebungsleitung.java
@@ -1,2 +1,50 @@
-public class Uebungsleitung {
+import java.util.Random;
+
+public class Uebungsleitung extends Thread {
+	private static Random random=new Random();
+	private Buffer in, out;
+	private static Count count=null;
+
+	public Uebungsleitung(Buffer bin, Buffer bout) {
+		in=bin;
+		out=bout;
+
+		if(count==null) {
+			count=new Count(1);
+		}
+	}
+
+	public void run() {
+		while(count.getn(0)<Helper.NEXAMS) {
+			try {
+				count.inc(0);
+				if(count.getn(0)>Helper.NEXAMS)
+					break;
+				Klausur k=in.consume();
+				k.setNote(Korrekturschema.note(k.getGesamtpunktzahl()));
+				int[] p=k.getPunkte();
+				if(random.nextInt(10)==0) {
+					int cn=random.nextInt(Helper.NTASKS);
+					for(int i=0; i<Helper.NTASKS; i++) {
+						if(i==cn)
+							switch(random.nextInt(2)) {
+							case 0: if(k.getPunkte()[i]>0) k.setZweitkorrektur(i, p[i]-1);
+								break;
+							case 1: if(k.getPunkte()[i]<7) k.setZweitkorrektur(i, p[i]+1);
+								break;
+							}
+						else
+							k.setZweitkorrektur(i, p[i]);
+					}
+				} else {
+					for(int i=0; i<Helper.NTASKS; i++)
+						k.setZweitkorrektur(i, p[i]);
+				}
+				out.produce(k);
+			}
+			catch(InterruptedException e) {
+				System.exit(2);
+			}
+		}
+	}
 }
